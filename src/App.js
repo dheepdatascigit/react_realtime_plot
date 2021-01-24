51f3e6e8a58f7ea1644414fdbd303ed6d479da67
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-01-24 21:15:59 +1100

    initila react app template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                return <h1>Hello, {this.props.name}</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default App
